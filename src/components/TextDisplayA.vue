89b9af6ba813e0baa66f1598ea67f0a9720e4fda
Zhyvotenko Oleksandr <oleksandr.zhyvotenko@devico.io>
2025-02-19 13:05:29 +0200

    init

---
+++ src/components/TextDisplayA.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-a">
+    <h2>Package A Content</h2>
+    <p>This is text from Package A</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextDisplayA",
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 const { defineConfig } = require('@vue/cli-service')
+const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  // publicPath: 'http://localhost:8081/',
+  publicPath: 'https://package-a.web.app/',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'packageA',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './TextA': './src/components/TextDisplayA.vue'
+        },
+        shared: {
+          vue: {
+            singleton: true,
+            eager: true,
+            requiredVersion: '^3.2.13'
+          }
+        }
+      })
+    ]
+  },
+  devServer: {
+    port: 8081,
+    hot: true
+  }
 })
